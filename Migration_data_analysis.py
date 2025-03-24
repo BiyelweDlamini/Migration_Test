d61f695677e3575dbc76d18d74b35cde3f71f415
BiyelweDlamini <122216660+BiyelweDlamini@users.noreply.github.com>
2025-03-24 16:53:16 +0200

    initial commit

---
+++ Migration_data_analysis.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+#initialize file path and add the r prefix to prefix the string to let Python know that it is a raw string (to avoid the backlashes being seen as escape sequences)
+migration_file_path = r'C:\Users\biyel\OneDrive\Desktop\Project_2025\Migration_Test\IndianMigrationHistory.csv'
+migration_data = pd.read_csv(migration_file_path)
+print(migration_data.columns)
+
+y = migration_data.2000[2000]
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Migration_Test
  Using data downaloaded from kaggle to analyze using a submodule from sklearn
+
+dataset source: The dataset was downloaded from Kaggle and was posted by user RAJA CSP
+dataset link: https://www.kaggle.com/datasets/rajacsp/indian-migration-history
+ 
