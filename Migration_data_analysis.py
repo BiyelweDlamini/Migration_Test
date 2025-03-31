14f2cade7dbfbf0093896aba16c4e41af7ed37da
BiyelweDlamini <122216660+BiyelweDlamini@users.noreply.github.com>
2025-03-31 16:11:45 +0200

    Feature selection

--- Migration_data_analysis.py
+++ Migration_data_analysis.py
@@ -11,7 +11,19 @@ from sklearn.model_selection import train_test_split
 
 #initialize file path and add the r prefix to prefix the string to let Python know that it is a raw string (to avoid the backlashes being seen as escape sequences)
 migration_file_path = r'C:\Users\biyel\OneDrive\Desktop\Project_2025\Migration_Test\IndianMigrationHistory.csv'
+
+#read the csv file
+
 migration_data = pd.read_csv(migration_file_path)
+
+#print all the columns in the csv file
+
 print(migration_data.columns)
 
-y = [[2000[2000]]] 
+#set the targert
+
+y = [["2000[2000]"]] 
+
+#Select the features
+
+
