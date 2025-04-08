2a551180b30916c4786ee0ef590d7b4af4cd3281
BiyelweDlamini <122216660+BiyelweDlamini@users.noreply.github.com>
2025-04-08 09:14:30 +0200

    changed the variable naming convernsion
    changed the naming conversion to Carmel case

--- Migration_data_analysis.py
+++ Migration_data_analysis.py
@@ -10,32 +10,34 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 #initialize file path and add the r prefix to prefix the string to let Python know that it is a raw string (to avoid the backlashes being seen as escape sequences)
-migration_file_path = r'C:\Users\biyel\OneDrive\Desktop\Project_2025\Migration_Test\IndianMigrationHistory.csv'
+migrationFilePath = r'C:\Users\biyel\OneDrive\Desktop\Project_2025\Migration_Test\IndianMigrationHistory.csv'
 
 #read the csv file
 
-migration_data = pd.read_csv(migration_file_path)
+migrationData = pd.read_csv(migrationFilePath)
 
 #print all the columns in the csv file
 
-print(migration_data.columns)
+print(migrationData.columns)
 
 #set the targert
 
-y = migration_data['2000[2000]']
+y = migrationData['2000[2000]']
 
 #Selecting the features
 
-migration_features = ['Country Origin Name', 'Country Origin Code',
+migrationFeatures = ['Country Origin Name', 'Country Origin Code',
                       'Migration by Gender Name', 'Migration by Gender Code',
                       'Country Dest Name', 'Country Dest Code', '1960 [1960]', 
                       '1970 [1970]','1980 [1980]', '1990 [1990]']
 
 #Storing the features in the x variable
 
-X = migration_data[migration_features]
+X = migrationData[migrationFeatures]
 
 #Splitting the data from the CSV into training data and vcalidation data 
 
-trainX, valX, trainy, valy = train_test_split(X, y, random_state = 0)
+#trainX, valX, trainy, valy = train_test_split(X, y, random_state = 0)
+
+#migration_model
 
